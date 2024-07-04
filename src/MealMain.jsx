abc19c8491eb4cc1c12015852a635d9762d64aff
Boikhutso Mokoka <boikhuso.m@gmail.com>
2024-07-04 12:04:15 +0200

    Added Axios import and API call to fetch data in MealMain component.

--- src/MealMain.jsx
+++ src/MealMain.jsx
@@ -1,7 +1,25 @@
-import React from "react";
+import axios from "axios";
+import { useEffect, useState } from "react";
 
 function MealMain() {
-  return <div>MealMain</div>;
+  const [items, setItems] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("https://www.themealbd.com/api/json/v1/1/filter.php?c=Seafood")
+      .then((res) => {
+        setItems(res.data.meal);
+      })
+      .catch((err) => {
+        console.log(error);
+      });
+  }, []);
+
+  return (
+    <>
+      <section className="card"></section>
+    </>
+  );
 }
 
 export default MealMain;
